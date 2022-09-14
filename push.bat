ecc2e96ece6b4abf534aa496c3dc4689d3f56afd
CIT142F-17@M307.UC-BCF.EDU.PH <CIT142F-17@M307.UC-BCF.EDU.PH>
2022-09-14 16:43:16 +0800

    -batch upload

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo off
+git remote set-url origin https://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@github.com/roseen/cit14-17.git
+pause
+git add .
+git commit -m -"batch upload"
+git push
+pause
