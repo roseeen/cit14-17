8602ab54d895ddcc53bf4355902321d97ab4b362
CIT142F-17@m307.uc-bcf.edu.ph <CIT142F-17@m307.uc-bcf.edu.ph>
2022-09-14 17:06:45 +0800

    -batch upload

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 @echo off
-git remote set-url origin https://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@github.com/roseeen/cit14-17.git
+git remote set-url origin https://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@github.com/roseeen/cit14-17.git
 pause
 git add .
-git commit -m "batch upload"
+git commit -m -"batch upload"
 git push
 pause
